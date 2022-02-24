32c09b704c25b6f985743bd4889805d998886260
KhacThien <94694945+KhaccThienn@users.noreply.github.com>
2022-02-24 19:38:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+_changed = () => {
+    var img_value = document.getElementById('img_selector').value;
+    var myimg = document.getElementById('img');
+    switch (img_value) {
+        case '1':
+            myimg.src = "../Data/yae_miko_1.jpg"
+            break;
+        case '2':
+            myimg.src = "../Data/yae_miko_2.jpg"
+            break;
+        case '3':
+            myimg.src = "../Data/yae_miko_3.jpg"
+            break;
+        case '4':
+            myimg.src = "../Data/yae_miko_4.jpg"
+            break;
+        case '5':
+            myimg.src = "../Data/yae_miko_5.jpg"
+            break;
+    }
+}
